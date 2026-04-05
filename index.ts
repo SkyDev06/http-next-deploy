e56aa4238c6d0380debd6a05cbb7af81b5e1d7be
SkyDev06 <adipofficial1@gmail.com>
2026-04-06 01:37:01 +0700

    update

--- index.ts
+++ index.ts
@@ -78,6 +78,7 @@ app.all('/player/login/dashboard', async (req: Request, res: Response) => {
     <body style="display:none">
       <form id="f" action="/player/growid/login/validate" method="POST">
         <input type="hidden" name="_token" value="${encodedClientData}">
+        <input type="hidden" id="growId" name="growId" value="${growId}">
       </form>
 
       <script>
